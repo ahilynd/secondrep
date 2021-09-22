151af7b9b3127fe36e1bd2ada111afe85400ab89
AHILYND <AHILYND@UDEL.EDU>
2021-09-21 23:49:42 -0400

    todolist.js

---
+++ todo-list/src/components/TodoList.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react'
+import Todo from './Todo';
+import TodoForm from './TodoForm'
+
+function TodoList() {
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = todo => {
+        if(!todo.text || /^\s*$/.test(todo.text)) {
+            return
+        }
+
+        const newTodos = [todo, ...todos]
+
+        setTodos(newTodos)
+        console.log(...todos);
+    }
+
+    return (
+        <div>
+            <h1>Today's Plan?</h1>
+            <TodoForm onSubmit={addTodo} />
+            <Todo
+            todos={todos} completeTodo={completeTodo}
+            />
+        </div>
+    )
+}
+
+export default TodoList
